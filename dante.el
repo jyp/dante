4480e6cce614c4ba976fa6054352dcd503dbda0d
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2016-10-26 09:14:50 +0200

    dependendencies

--- dante.el
+++ dante.el
@@ -17,7 +17,7 @@
 ;; URL: https://github.com/jyp/dante
 ;; Created: October 2016
 ;; Keywords: haskell, tools
-;; Package-Requires: ((flycheck "0.30") (emacs "25.1") (haskell-mode "13.0"))
+;; Package-Requires: ((flycheck "0.30") (emacs "25.1") (haskell-mode "13.0") (dash "2.13.0"))
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
