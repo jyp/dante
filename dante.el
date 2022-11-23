81ed58540cf23658be51450c99992feb4494b5ab
Uladzimir Stsepchanka <st.uladzimir@protonmail.com>
2022-11-23 12:00:26 +0300

    Use :load! and :reload! instead of :l! :r!
    Unlike shortcuts works in older GHCs

--- dante.el
+++ dante.el
@@ -326,8 +326,8 @@ and over."
       (lcr-call dante-async-call (if interpret ":set -fbyte-code" ":set -fobject-code"))
       (with-current-buffer buffer
         (setq-local dante-status 'loading)
-        (dante-async-write (if same-target ":r!"
-                             (concat ":l! " (if interpret "*" "") (dante-local-name fname))))
+        (dante-async-write (if same-target ":reload!"
+                             (concat ":load! " (if interpret "*" "") (dante-local-name fname))))
         (cl-destructuring-bind (_status err-messages _loaded-modules)
             (lcr-call dante-load-loop "" nil err-fn)
           (setq dante-loaded-file src-fname)
